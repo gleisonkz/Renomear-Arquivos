e338c75dd6ec5f1682ca4f01a20aca5069e50835
gleisonkz <gleisonsubzerokz@gmail.com>
2019-03-25 23:30:03 -0300

    Aplicação simples para renomear arquivos.

---
+++ RenomearArquivos/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RenomearArquivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] arquivos = Directory.GetFiles(@"C:\Users\Gleison\Downloads\TESTE RENAME");
+            string dirsaida = @"C:\Users\Gleison\Downloads\TESTE RENAME";
+
+            for (int i = 0; i < arquivos.Length; i++)
+            {
+                var files = new FileInfo(arquivos[i]);
+                files.CopyTo(Path.Combine(dirsaida, files.Name.Replace("QUERO", "").Trim()));
+
+            }
+
+            MessageBox.Show("Movimentação e rename concluído");
+        }
+    }
+}
